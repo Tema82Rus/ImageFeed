315c5deacca35c5a35a76bec1a1506d4de8039c4
Tema82Rus <Fenix23245@yandex.ru>
2026-01-26 20:23:54 +0300

    add: DataSource & Delegate for ImagesListViewController

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Artem Yaroshenko on 28.12.2025.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
