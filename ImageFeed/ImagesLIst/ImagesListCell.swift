b975da197c36f8f3df131b3368877e1551c06824
Tema82Rus <Fenix23245@yandex.ru>
2026-01-27 14:41:31 +0300

    style: add marks

--- ImageFeed/ImagesLIst/ImagesListCell.swift
+++ ImageFeed/ImagesLIst/ImagesListCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    // MARK: - Property
     static let reuseIdentifier = "ImagesListCell"
     
 }
--- ImageFeed/ImagesLIst/ImagesListViewController.swift
+++ ImageFeed/ImagesLIst/ImagesListViewController.swift
@@ -8,19 +8,20 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-
+    // MARK: - Property
     @IBOutlet private var tableView: UITableView!
-    
+    // MARK: - Function
     func configCell(for cell: ImagesListCell) {
         
     }
-    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 200
     }
 }
 
+    // MARK: - Extension
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
