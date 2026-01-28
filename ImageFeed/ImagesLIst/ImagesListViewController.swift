43e752ecccb4206c2f19dc9f2110ec6c61160a46
Tema82Rus <Fenix23245@yandex.ru>
2026-01-29 01:38:35 +0300

    fix(main): title for UIButton
    refactor(ImagesListViewController): after review

--- ImageFeed/ImagesLIst/ImagesListViewController.swift
+++ ImageFeed/ImagesLIst/ImagesListViewController.swift
@@ -23,7 +23,6 @@ final class ImagesListViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.rowHeight = 200
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
@@ -31,7 +30,7 @@ final class ImagesListViewController: UIViewController {
     // MARK: - Extension
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return photosName.count
+        photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -73,7 +72,7 @@ extension ImagesListViewController {
         cell.dateLabel.text = dateFormatter.string(from: Date())
         
         let isLiked = indexPath.row % 2 == 0
-        let likeImage = isLiked ? UIImage(named: "FavoritesActive") : UIImage(named: "FavoritesNoActive")
+        let likeImage = isLiked ? UIImage(resource: .favoritesActive) : UIImage(resource: .favoritesNoActive)
         cell.likeButton.setImage(likeImage, for: .normal)
     }
 }
