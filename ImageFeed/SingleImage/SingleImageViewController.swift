a81b19f7addcce6fb41534c2eb8ecd5922460e23
Tema82Rus <Fenix23245@yandex.ru>
2026-02-04 23:46:50 +0300

    add: Push for SingleImageViewController

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Artem Yaroshenko on 04.02.2026.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
