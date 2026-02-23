1b278b641afa173f68fc5f13ab5f6dc00ad8113f
Tema82Rus <Fenix23245@yandex.ru>
2026-02-23 13:22:00 +0300

    fix(Constants)

--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -11,6 +11,6 @@ enum Constants {
     static let accessKey = "JtSgGa5h61PAN_YklGEpz4Q4komNPd8j_34gczoPo8w"
     static let secretKey = "J_NLFcfJV8Vw0TVsA_Emnjc4XkDXWUp4asEeK6-KtuQ"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
-    static let accessScope = "public"
+    static let accessScope = "public+read_user+write_likes"
     static let defaultBaseURLString: String = "https://api.unsplash.com/"
 }
