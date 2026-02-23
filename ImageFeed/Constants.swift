54cd6c0da271b26110aa30c9a2111743a2555544
Tema82Rus <Fenix23245@yandex.ru>
2026-02-23 13:18:34 +0300

    add(Constants)

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Artem Yaroshenko on 23.02.2026.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey = "JtSgGa5h61PAN_YklGEpz4Q4komNPd8j_34gczoPo8w"
+    static let secretKey = "J_NLFcfJV8Vw0TVsA_Emnjc4XkDXWUp4asEeK6-KtuQ"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public"
+    static let defaultBaseURLString: String = "https://api.unsplash.com/"
+}
